e7595919987c18039ec15abb327e2e36f26bb4b7
ThibGit99 <155988433+ThibGit99@users.noreply.github.com>
2024-12-31 13:47:00 +0100

    Delete Bulletin-Board directory

--- Bulletin-Board/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import '@fontsource/dm-sans/400.css';
-import ApolloProvider from './ApolloProvider';
-
-ReactDOM.render(ApolloProvider, document.getElementById('root'));
-
-reportWebVitals();
